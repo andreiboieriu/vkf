38d89c07fd3b68a1f476b870633d711cdaa51a92
RCX777 <robertcnst02@gmail.com>
2024-03-06 01:12:08 +0200

    adds flake support for the nix package manager
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            cmake
+
+            glfw
+            shaderc
+            vulkan-headers
+            vulkan-loader
+            vulkan-validation-layers
+            freetype
+          ];
+          VULKAN_SDK = "${vulkan-headers}";
+          VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+          LD_LIBRARY_PATH="${glfw}/lib:${freetype}/lib:${vulkan-loader}/lib:${vulkan-validation-layers}/lib";
+
+        };
+      }
+    );
+}
+
--- unixBuild.sh
+++ unixBuild.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+CC=$(which clang)
+CXX=$(which clang++)
+
 mkdir -p build
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+cmake -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 make && make Shaders && ./VulkanEngine
 cd ..
